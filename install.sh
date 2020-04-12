0c81901b9654e7f65cf2a871f3c3ee2a218eef39
vatsa287 <i.mnshreevatsa@gmail.com>
2020-04-12 20:45:47 +0530

    installation file

---
+++ install.sh
@@ -0,0 +1,21 @@
+echo "Starting to install Glut Packages"
+echo "->"
+echo "-->"
+echo "--->"
+echo "---->"
+echo "----->"
+echo "------>"
+echo "------->"
+echo "-------->"
+echo "---------->"
+echo "----------->"
+echo "------------>"
+echo "------------->"
+echo "-------------->"
+echo "---------------------------->"
+
+sudo apt-get install freeglut3-dev
+
+echo "<----------------------------"
+
+echo "Installation complete. OpenGL ready to use."
